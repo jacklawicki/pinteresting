b955dbb820f9ec43506720c9a2f3ac21757fb368
jacklawicki <jack.lawicki@gmail.com>
2015-02-14 09:39:17 +1100

    add pin user associations

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :correct_user, only:[:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   # GET /pins
   # GET /pins.json
   def index
@@ -59,6 +60,11 @@ class PinsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorise to edit this Pin." if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213021318) do
+ActiveRecord::Schema.define(version: 20150213091707) do
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
